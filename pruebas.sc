6fd0c8b115dcb381f6ba2412ed8baea9d13d9a8b
Mónica <cortes.monica@correounivalle.edu.co>
2025-04-03 09:42:27 -0500

    definir maniobra

--- pruebas.sc
+++ pruebas.sc
@@ -6,3 +6,10 @@ val e2= aplicarMovimiento( e1 , Uno( 2 ) )
 val e3 = aplicarMovimiento( e2 , Dos( 3 ) )
 val e4 = aplicarMovimiento( e3 , Dos( -1 ))
 val e5 = aplicarMovimiento( e4 , Uno( -2))
+
+
+val e = (List('a', 'b'), List('c'), List('d'))
+aplicarMovimientos(e, List(Uno(1), Dos(1), Uno(-2)))
+
+
+val d1 = definirManiobra(List('a','b','c','d'), List('d', 'b','c', 'a'))
--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -2,9 +2,12 @@ package object ManiobrasTrenes {
   type Vagon = Any
   type Tren = List[Vagon]
   type Estado = (Tren, Tren, Tren)
+  type Maniobra = List[Movimiento]
 
   trait Movimiento
+
   case class Uno(n: Int) extends Movimiento
+
   case class Dos(n: Int) extends Movimiento
 
   def aplicarMovimiento(e: Estado, m: Movimiento): Estado = {
@@ -34,4 +37,68 @@ package object ManiobrasTrenes {
     movs.foldLeft(List(e)) { (estados, mov) =>
       estados :+ aplicarMovimiento(estados.last, mov)
     }
+
+  def definirManiobra(t1: Tren, t2: Tren): Maniobra = {
+    def aux(principal: Tren, uno: Tren, dos: Tren, objetivo: Tren, acc: Maniobra): Maniobra = {
+      (objetivo.isEmpty, uno.isEmpty, dos.isEmpty) match {
+        case (true, true, true) => acc.reverse
+
+        case (false, _, _) =>
+          val next = objetivo.head
+          (uno.contains(next), dos.contains(next)) match {
+            case (true, _) =>
+              val index = uno.indexOf(next)
+              if (index == 0) {
+                val newAcc = Uno(-1) :: acc
+                aux(next :: principal, uno.tail, dos, objetivo.tail, newAcc)
+              } else {
+                val moveToPrincipal = Uno(-index)
+                val newState1 = aplicarMovimiento((principal, uno, dos), moveToPrincipal)
+                val moveToDos = Dos(index)
+                val newState2 = aplicarMovimiento(newState1, moveToDos)
+                aux(newState2._1, newState2._2, newState2._3, objetivo,
+                  moveToDos :: moveToPrincipal :: acc)
+              }
+
+            case (_, true) =>
+              val index = dos.indexOf(next)
+              if (index == 0) {
+                val newAcc = Dos(-1) :: acc
+                aux(next :: principal, uno, dos.tail, objetivo.tail, newAcc)
+              } else {
+                val moveToPrincipal = Dos(-index)
+                val newState1 = aplicarMovimiento((principal, uno, dos), moveToPrincipal)
+                val moveToUno = Uno(index)
+                val newState2 = aplicarMovimiento(newState1, moveToUno)
+                aux(newState2._1, newState2._2, newState2._3, objetivo,
+                  moveToUno :: moveToPrincipal :: acc)
+              }
+
+            case _ => acc.reverse
+          }
+
+        case _ =>
+          (uno.nonEmpty, dos.nonEmpty) match {
+            case (true, _) =>
+              val newAcc = Uno(-uno.length) :: acc
+              val newState = aplicarMovimiento((principal, uno, dos), Uno(-uno.length))
+              aux(newState._1, newState._2, newState._3, objetivo, newAcc)
+
+            case (_, true) =>
+              val newAcc = Dos(-dos.length) :: acc
+              val newState = aplicarMovimiento((principal, uno, dos), Dos(-dos.length))
+              aux(newState._1, newState._2, newState._3, objetivo, newAcc)
+
+            case _ => acc.reverse
+          }
+      }
+    }
+
+    val movimientosIniciales = List(Uno(t1.length))
+    val estadoInicial = aplicarMovimiento((t1, Nil, Nil), Uno(t1.length))
+
+    aux(estadoInicial._1, estadoInicial._2, estadoInicial._3, t2, movimientosIniciales)
+  }
+
+
 }
